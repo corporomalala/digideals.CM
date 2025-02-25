9d320598bcbadc510de5dd66a637c8254b56559f
Madam Ella <madamellamalala@gmail.com>
2025-02-25 13:28:56 +0200

    .

---
+++ _includes/assets/script_community-catalog.dependencies.js
@@ -0,0 +1,5 @@
+		<!--
+		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js"></script>
+		-->
+		<div id="fb-root"></div>
+		<script async defer crossorigin="anonymous" src="https://connect.facebook.net/en_US/sdk.js#xfbml=1&version=v22.0&appId=1015230857299484"></script>
