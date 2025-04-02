cc6364b326d10d79c043f9fb4c5b4b81fa2f8c21
Madam Ella <madamellamalala@gmail.com>
2025-04-02 12:44:17 +0200

    .

--- _includes/assets/script_GLOBAL-community.js
+++ _includes/assets/script_GLOBAL-community.js
@@ -1,13 +1,12 @@
 /*** GLOBAL COMMUNITY ***/
-	/*** cookies ***/
 	var vCookies,
 		vUserID,
-		vPage;
+		vPage, vLastPage;
 
 //	setCookie(19, "Hello!");
 	function setCookie(uID, uState) {
 		if(vUserID == null) {
-			document.cookie = "DigiDeals: " + uID + "=" + uState + "; expires=Thu, 01, Jan 2099 00:00:01 GMT;";
+			document.cookie = "DigiDeals-USER: " + uID + "=" + uState + "; path=/; expires=Thu, 01, Jan 2099 00:00:01 GMT;";
 			
 			vUserID = "here";
 		}
@@ -19,9 +18,9 @@
 		var vCookie;
 		for(var i = 0; i < vCookies.length; i++) {
 			vCookie = vCookies[i].trim();
-			if(vCookie.startsWith("DigiDeals: ")) {
+			if(vCookie.startsWith("DigiDeals-USER: ")) {
 				// User/Client is here.
-				vUserID = vCookie.replace("DigiDeals: ", "");
+				vUserID = vCookie.replace("DigiDeals-USER: ", "");
 				vUserID = vUserID.split("=", 1);
 
 //				alert(document.getElementsByClassName("js-uMessage"));
@@ -34,10 +33,28 @@
 			vPage = document.querySelector("html").getAttribute("attr-page");
 			
 			if(vPage != "community") {
+				document.cookie = "DigiDeals-HREF: " + vPage + "; path=/; expires=Thu, 01, Jan 2099 00:00:01 GMT;";
+
 				// Redirect new clients to log in page
+				window.location.href = "/community/";
 			} else {
 				// Click log in button
 				alert("Hello, New Client! Kindly log in!");
+				
+				if(vPage != vLastPage) {
+					for(var i = 0; i < vCookies.length; i++) {
+						vCookie = vCookies[i].trim();
+						if(vCookie.startsWith("DigiDeals-HREF: ")) {
+							vLastPage = vCookie.replace("DigiDeals-HREF: ", "");
+							vLastPage = vLastPage.split(";", 1);
+							alert("Redirecting to: https://localhost:4000/" + vLastPage + "/");
+
+			//				alert(document.getElementsByClassName("js-uMessage"));
+			//				document.getElementsByClassName("js-uMessage").textContent = "Welcome back, Client N* ";
+			//				alert("Welcome back, Client N* " + vUserID);
+						}
+					}
+				}
 			}
 		}
 	}
